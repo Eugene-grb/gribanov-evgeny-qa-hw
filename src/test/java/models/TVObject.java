b6041a339d02dcc0bb986a43e4a672611a511bd7
Evgeny Gribanov <gribanoveu@yandex.ru>
2021-09-07 17:40:17 +0300

    hw04

--- src/test/java/models/TVObject.java
+++ src/test/java/models/TVObject.java
@@ -1,4 +1,48 @@
 package models;
 
+import models.valueObjects.Company;
+import models.valueObjects.Diagonal;
+import models.valueObjects.Illumination;
+import models.valueObjects.RefreshRate;
+
 public class TVObject {
+
+    private Company company;
+    private Diagonal diagonal;
+    private Illumination illumination;
+    private RefreshRate refreshRate;
+
+    public TVObject() {}
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Diagonal getDiagonal() {
+        return diagonal;
+    }
+
+    public void setDiagonal(Diagonal diagonal) {
+        this.diagonal = diagonal;
+    }
+
+    public Illumination getIllumination() {
+        return illumination;
+    }
+
+    public void setIllumination(Illumination illumination) {
+        this.illumination = illumination;
+    }
+
+    public RefreshRate getRefreshRate() {
+        return refreshRate;
+    }
+
+    public void setRefreshRate(RefreshRate refreshRate) {
+        this.refreshRate = refreshRate;
+    }
 }
--- src/test/java/models/TVObjectBuilder.java
+++ src/test/java/models/TVObjectBuilder.java
@@ -1,4 +1,35 @@
 package models;
 
+import models.valueObjects.Company;
+import models.valueObjects.Diagonal;
+import models.valueObjects.Illumination;
+import models.valueObjects.RefreshRate;
+
 public class TVObjectBuilder {
+
+    private final Company company;
+    private final Diagonal diagonal;
+    private final Illumination illumination;
+    private final RefreshRate refreshRate;
+
+    public TVObjectBuilder(
+            Company company,
+            Diagonal diagonal,
+            Illumination illumination,
+            RefreshRate refreshRate
+    ) {
+        this.company = company;
+        this.diagonal = diagonal;
+        this.illumination = illumination;
+        this.refreshRate = refreshRate;
+    }
+
+    public TVObject build() {
+        TVObject tvObject = new TVObject();
+        tvObject.setCompany(this.company);
+        tvObject.setDiagonal(this.diagonal);
+        tvObject.setIllumination(this.illumination);
+        tvObject.setRefreshRate(this.refreshRate);
+        return tvObject;
+    }
 }
